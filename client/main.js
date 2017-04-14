ec587f1be20ad4bcdd5629632582f792c3156e8d
Benjamin Sampson <bensampsondev@gmail.com>
2017-04-14 21:49:08 +1000

    Init commit

---
+++ client/main.js
@@ -0,0 +1,15 @@
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+import { Tracker } from 'meteor/tracker';
+
+import { routes, onAuthChange } from '../imports/routes/routes';
+import '../imports/startup/simple-schema-configuration';
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  onAuthChange(isAuthenticated);
+});
+
+Meteor.startup(() => {
+  render(routes, document.getElementById('app'));
+});
